addd33ceb9ec6cd2c333844766db6a09b861be61
Nick Arocho <nick@boldlyfine.com>
2018-02-16 10:53:57 -0800

    add FullRoster

--- src/index.js
+++ src/index.js
@@ -33,6 +33,20 @@ const Schedule = () => (
     </div>
 )
 
+const FullRoster = () => (
+    <div>
+        <ul>
+            {
+                PlayerAPI.all().map(p => (
+                    <li key={p.number}>
+                        <Link to={`/roster/${p.number}`}>{p.name}</Link>
+                    </li>
+                ))
+            }
+        </ul>
+    </div>
+)
+
 const Home = () => (
     <div>Home Component</div>
 )
