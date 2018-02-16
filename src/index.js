9f9e6a5a7e8bc96356f7a4ea3cafb366c4de7479
Nick Arocho <nick@boldlyfine.com>
2018-02-16 10:58:50 -0800

    create Player component

--- src/index.js
+++ src/index.js
@@ -47,6 +47,22 @@ const FullRoster = () => (
     </div>
 )
 
+const Player = (props) => {
+    const player = PlayerAPI.get(
+        parseInt(props.match.params.number, 10)
+    )
+    if (!player) {
+        return <div>Sorry, but the player was not found</div>
+    }
+    return (
+        <div>
+            <h1>{player.name} (#{player.number})</h1>
+            <h2>Position: {player.position}</h2>
+            <Link to='/roster'>Back</Link>
+        </div>
+    )
+}
+
 const Home = () => (
     <div>Home Component</div>
 )
